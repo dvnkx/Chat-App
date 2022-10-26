4f67edfbf7c589ab23ab620659184f31c9d4c443
Bogdan <dvnkxxxx@gmail.com>
2022-10-26 13:18:00 +0300

    fix walkthrough pull reaquests

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Walkthrough} from './screens/Walkthrough';
+import {Login} from './screens/Login';
+
+type RootStackParamList = {
+  Walkthrough: {name: string};
+  Login: {name: string} | undefined;
+};
+
+export type NavigationProps = StackNavigationProp<RootStackParamList>;
+
+export type Props = NativeStackScreenProps<RootStackParamList, 'Walkthrough'>;
+
+const App = () => {
+  const RootStack = createStackNavigator<RootStackParamList>();
+
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen
+          name="Walkthrough"
+          component={Walkthrough}
+          options={{headerShown: false}}
+        />
+        <RootStack.Screen name="Login" component={Login} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
